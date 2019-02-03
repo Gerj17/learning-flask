0d88ecd266708263c7604bc7d52f0c4b4af2539c
Gerard <jonesgerard25@gmail.com>
2019-02-03 12:29:40 -0400

    On branch master deleted all files to restart

--- sched/manage.py
+++
@@ -1,8 +0,0 @@
-from flask.exe.script import Manager 
-from sched.app import app 
-
-manager = Manager(app)
-app.config["DEBUG"] = True     # ensure debugger wll load 
-
-if __name__ == "__main__":
-    manager.run()
